94315abbe913007c5167a42f037755bbcd5198cd
AnaisRevellat <anaisrevellat@gmail.com>
2022-08-27 21:24:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Helllooooo</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +24,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
