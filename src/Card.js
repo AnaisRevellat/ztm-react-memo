5af6c8fdf11fb775b79b209460819fd48ca6c7e6
AnaisRevellat <anaisrevellat@gmail.com>
2022-08-27 21:59:36 +0200

    cards_creation

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img src="robot" alt="" />
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.do@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
